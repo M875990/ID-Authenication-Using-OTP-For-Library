36290e5dbde2c7933cf2c681360985f08eab3274
MADHU K S <97436224+M875990@users.noreply.github.com>
2024-08-01 20:22:01 +0530

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+            <div class="d-sm-flex justify-content-center justify-content-sm-between">
+              <span class="text-muted d-block text-center text-sm-left d-sm-inline-block" style="color: blue;">ID AUTHENTICATION USING OTP FOR LIBRARY</span>
+              
+            </div>
+          </footer>
