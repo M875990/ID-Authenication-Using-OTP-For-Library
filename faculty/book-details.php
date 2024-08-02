d2d0d8382cde72cfd09b1611f2692dd407e875d5
MADHU K S <97436224+madhugowda-1819@users.noreply.github.com>
2024-08-02 11:50:04 +0530

    Add files via upload

---
+++ faculty/book-details.php
@@ -0,0 +1,135 @@
+<?php
+session_start();
+//error_reporting(0);
+include('includes/dbconnection.php');
+if (strlen($_SESSION['sturecmsstuid']==0)) {
+  header('location:logout.php');
+  } else{
+   
+  ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+   
+    <title>View Book Details</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="vendors/simple-line-icons/css/simple-line-icons.css">
+    <link rel="stylesheet" href="vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <link rel="stylesheet" href="vendors/select2/select2.min.css">
+    <link rel="stylesheet" href="vendors/select2-bootstrap-theme/select2-bootstrap.min.css">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="css/style.css" />
+    
+  </head>
+  <body>
+    <div class="container-scroller">
+      <!-- partial:partials/_navbar.html -->
+     <?php include_once('includes/header.php');?>
+      <!-- partial -->
+      <div class="container-fluid page-body-wrapper">
+        <!-- partial:partials/_sidebar.html -->
+      <?php include_once('includes/sidebar.php');?>
+        <!-- partial -->
+        <div class="main-panel">
+          <div class="content-wrapper">
+            <div class="page-header">
+              <h3 class="page-title"> View Faculty Book Details</h3>
+              <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                  <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
+                  <li class="breadcrumb-item active" aria-current="page"> View Book Details</li>
+                </ol>
+              </nav>
+            </div>
+            <div class="row">
+          
+              <div class="col-12 grid-margin stretch-card">
+                <div class="card">
+                  <div class="card-body">
+                    
+                    <table border="1" class="table table-bordered mg-b-0">
+                      <?php
+$sid=$_SESSION['sturecmsstuid'];
+$sql="SELECT tblfacultybook.FacultyName,tblfacultybook.Gender,
+tblfacultybook.FacultyID,tblfacultybook.Branch,tblfacultybook.BookId,tblfacultybook.BookName,tblfacultybook.BookAuthorName,tblfacultybook.Borrowdate,tblfacultybook.Duedate from tblfacultybook  where tblfacultybook.FacultyID=:sid";
+$query = $dbh -> prepare($sql);
+$query->bindParam(':sid',$sid,PDO::PARAM_STR);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $row)
+{               ?>
+ <tr align="center" class="table-primary">
+<td colspan="4" style="font-size:20px;color:blue">Book Details</td></tr>
+
+    <tr class="table-striped">
+    <th>Faculty Name</th>
+    <td><?php  echo $row->FacultyName;?></td>
+     <th>Faculty ID</th>
+    <td><?php  echo $row->FacultyID;?></td>
+  </tr>
+  <tr class=" table-info">
+  <th>Branch</th>
+    <td><?php  echo $row->Branch;?> 
+    <th>Gender</th>
+    <td><?php  echo $row->Gender;?></td>
+</tr>
+    <tr class="table-warning">
+    <th>Book ID</th>
+    <td><?php  echo $row->BookId;?></td>
+    <th>Book Name</th>
+    <td><?php  echo $row->BookName;?></td>
+    </tr>
+    <tr class="table-danger">
+    <th>Book Author Name</th>
+    <td><?php  echo $row->BookAuthorName;?></td>
+    <th>Borrow Date</th>
+    <td><?php  echo $row->Borrowdate;?></td>
+  </tr>
+  <tr class="table-success">
+    <th>Due Date</th>
+    <td><?php  echo $row->Duedate;?></td>
+  </tr>
+  
+  <?php $cnt=$cnt+1;}} ?>
+</table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- content-wrapper ends -->
+          <!-- partial:partials/_footer.html -->
+         <?php include_once('includes/footer.php');?>
+          <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="vendors/js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <script src="vendors/select2/select2.min.js"></script>
+    <script src="vendors/typeahead.js/typeahead.bundle.min.js"></script>
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="js/off-canvas.js"></script>
+    <script src="js/misc.js"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page -->
+    <script src="js/typeahead.js"></script>
+    <script src="js/select2.js"></script>
+    <!-- End custom js for this page -->
+  </body>
+</html><?php }  ?>
---
+++ faculty/faculty-profile.php
@@ -0,0 +1,142 @@
+<?php
+session_start();
+//error_reporting(0);
+include('includes/dbconnection.php');
+if (strlen($_SESSION['sturecmsstuid']==0)) {
+  header('location:logout.php');
+  } else{
+   
+  ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+   
+    <title>View Faculty Profile</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="vendors/simple-line-icons/css/simple-line-icons.css">
+    <link rel="stylesheet" href="vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <link rel="stylesheet" href="vendors/select2/select2.min.css">
+    <link rel="stylesheet" href="vendors/select2-bootstrap-theme/select2-bootstrap.min.css">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="css/style.css" />
+    
+  </head>
+  <body>
+    <div class="container-scroller">
+      <!-- partial:partials/_navbar.html -->
+     <?php include_once('./includes/header.php');?>
+      <!-- partial -->
+      <div class="container-fluid page-body-wrapper">
+        <!-- partial:partials/_sidebar.html -->
+      <?php include_once('includes/sidebar.php');?>
+        <!-- partial -->
+        <div class="main-panel">
+          <div class="content-wrapper">
+            <div class="page-header">
+              <h3 class="page-title"> View Faculty Profile </h3>
+              <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                  <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
+                  <li class="breadcrumb-item active" aria-current="page"> View Faculty Profile</li>
+                </ol>
+              </nav>
+            </div>
+            <div class="row">
+          
+          <div class="col-12 grid-margin stretch-card">
+            <div class="card">
+              <div class="card-body">
+                
+                <table border="1" class="table table-bordered mg-b-0">
+                  <?php
+$sid=$_SESSION['sturecmsstuid'];
+$sql="SELECT tblfaculty.FacultyName,tblfaculty.FacultyEmail, tblfaculty.Branch,tblfaculty.Gender,tblfaculty.DOB,tblfaculty.FacultyID,tblfaculty.ContactNumber,tblfaculty.AlternateNumber,tblfaculty.Address,tblfaculty.UserName,tblfaculty.Password,tblfaculty.Image,tblfaculty.registrationDate from tblfaculty  where tblfaculty.FacultyID=:sid";
+$query = $dbh -> prepare($sql);
+$query->bindParam(':sid',$sid,PDO::PARAM_STR);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $row)
+{   ?>
+ <tr align="center" class="table-warning">
+<td colspan="4" style="font-size:20px;color:blue">Faculty Details</td></tr>
+
+    <tr class="table-info">
+    <th>Faculty Name</th>
+    <td><?php  echo $row->FacultyName;?></td>
+     <th>Faculty Email</th>
+    <td><?php  echo $row->FacultyEmail;?></td>
+  </tr>
+  <tr class=" table-danger">
+  <th>Branch</th>
+    <td><?php  echo $row->Branch;?> 
+    <th>Gender</th>
+    <td><?php  echo $row->Gender;?></td>
+</tr>
+    <tr class="table-warning">
+    <th>Faculty ID</th>
+    <td><?php  echo $row->FacultyID;?></td>
+    <th>Date of Birth</th>
+    <td><?php  echo $row->DOB;?></td>
+    </tr>
+    <tr class="table-danger">
+    <th>Contact Number</th>
+    <td><?php  echo $row->ContactNumber;?></td>
+    <th>Alternate Number</th>
+    <td><?php  echo $row->AlternateNumber;?></td>
+  </tr>
+  <tr class="table-primary">
+    <th>Address</th>
+    <td><?php  echo $row->Address;?></td>
+    <th>User Name</th>
+    <td><?php  echo $row->UserName;?></td>
+  </tr>
+  <tr class="table-warning">
+  <th>Profile Picture</th>
+    <td><img src="../admin/images/<?php echo $row->Image;?>"></td>
+  <th>Registration Date</th>
+    <td><?php  echo $row->registrationDate;?></td>
+    
+  </tr>
+  <?php $cnt=$cnt+1;}} ?>
+</table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- content-wrapper ends -->
+          <!-- partial:partials/_footer.html -->
+         <?php include_once('includes/footer.php');?>
+          <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="vendors/js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <script src="vendors/select2/select2.min.js"></script>
+    <script src="vendors/typeahead.js/typeahead.bundle.min.js"></script>
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="js/off-canvas.js"></script>
+    <script src="js/misc.js"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page -->
+    <script src="js/typeahead.js"></script>
+    <script src="js/select2.js"></script>
+    <!-- End custom js for this page -->
+  </body>
+</html><?php }  ?>
